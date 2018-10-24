ba734adb7e97bdc0cc9d0c23a8dff71e729af535
John J. Lee <jjlee.wustl.edu@gmail.com>
2018-10-24 16:08:50 -0500

    Stub for abstract factory pattern of mlpet.InstrumentKit

---
+++ src/+mlpowers/InstrumentKit.m
@@ -0,0 +1,19 @@
+classdef InstrumentKit < mlpet.InstrumentKit
+	%% INSTRUMENTKIT  
+
+	%  $Revision$
+ 	%  was created 18-Oct-2018 13:48:57 by jjlee,
+ 	%  last modified $LastChangedDate$ and placed into repository /Users/jjlee/MATLAB-Drive/mlpowers/src/+mlpowers.
+ 	%% It was developed on Matlab 9.4.0.813654 (R2018a) for MACI64.  Copyright 2018 John Joowon Lee.
+ 	
+	properties
+ 		
+ 	end
+
+	methods 
+		  
+  	end 
+
+	%  Created with Newcl by John J. Lee after newfcn by Frank Gonzalez-Morphy
+ end
+
